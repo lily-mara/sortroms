11207f8020b147175de4315b16c56c01ad20ef66
Nate Mara <natemara@gmail.com>
2014-09-20 20:55:28 -0400

    added package scaffolding

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+setup(
+	name='sortroms',
+	version='0.0.0',
+	description='Python package for sorting emulator ROM files',
+	url='http://github.com/natemara/sortroms',
+	author='Nate Mara',
+	author_email='natemara@gmail.com',
+	license='MIT',
+	packages=['sortroms'],
+	zip_safe=False
+)
