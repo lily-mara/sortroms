94e5681801c553e30bafd4802250187c60124865
Nate Mara <natemara@gmail.com>
2014-09-20 22:27:17 -0400

    update verson to 0.0.2

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 setup(
 	name='sortroms',
-	version='0.0.1',
+	version='0.0.2',
 	description='Python package for sorting emulator ROM files',
 	url='http://github.com/natemara/sortroms',
 	author='Nate Mara',
