e64aa079c5405f6ed5fbd6e327635cec944de3cf
Nate Mara <natemara@gmail.com>
2014-09-20 23:28:23 -0400

    update version for incompatable changes

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 setup(
 	name='sortroms',
-	version='0.0.5',
+	version='1.0.0',
 	description='Python package for sorting emulator ROM files',
 	url='http://github.com/natemara/sortroms',
 	author='Nate Mara',
