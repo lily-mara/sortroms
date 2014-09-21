f9b7760e53fb81153547b81a60641c909f0052ce
Nate Mara <natemara@gmail.com>
2014-09-20 23:21:32 -0400

    update version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 setup(
 	name='sortroms',
-	version='0.0.4',
+	version='0.0.5',
 	description='Python package for sorting emulator ROM files',
 	url='http://github.com/natemara/sortroms',
 	author='Nate Mara',
