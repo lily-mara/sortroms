88491f1649814ce411cbb7a0f09d736f2c737ad1
Nate Mara <natemara@gmail.com>
2014-09-20 22:51:21 -0400

    update setup to new version

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 setup(
 	name='sortroms',
-	version='0.0.2',
+	version='0.0.3',
 	description='Python package for sorting emulator ROM files',
 	url='http://github.com/natemara/sortroms',
 	author='Nate Mara',
 	author_email='natemara@gmail.com',
 	license='MIT',
 	packages=['sortrom'],
-	package_data={'': ['sortrom/*.json']},
+	package_data={'sortroms': ['sortrom/*.json']},
 	zip_safe=False
 )
