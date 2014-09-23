de027652a4bb12c6d1a4cb7bc85448c8c2a0d321
Nate Mara <natemara@gmail.com>
2014-09-23 12:51:54 -0400

    use argparse to get arguments from command line

--- sortroms/__main__.py
+++ sortroms/__main__.py
@@ -1,4 +1,19 @@
 from sortroms import main
+import argparse
+
+parser = argparse.ArgumentParser(
+	description='Sort emulator ROM files',
+	prog='sortroms'
+)
+
+parser.add_argument(
+	'folder',
+	metavar='DIR',
+	type=str,
+	nargs='?',
+	help='The ROM folder to sort.'
+)
 
 if __name__ == '__main__':
-	main()
+	args = parser.parse_args()
+	main(args)
