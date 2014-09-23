a2118d99eccb05bf20d4f9c551cd3d9898d08fe3
Nate Mara <natemara@gmail.com>
2014-09-23 13:55:08 -0400

    add command line entrypoint to init file

--- sortroms/__init__.py
+++ sortroms/__init__.py
@@ -1,3 +1,21 @@
 from __future__ import division, print_function, absolute_import
+import argparse
 
 from .organizer import main
+
+def command_line_entrypoint():
+	parser = argparse.ArgumentParser(
+		description='Sort emulator ROM files',
+		prog='sortroms'
+	)
+
+	parser.add_argument(
+		'folders',
+		metavar='DIR',
+		type=str,
+		nargs='*',
+		help='The ROM folders to sort (absolute)'
+	)
+
+	args = parser.parse_args()
+	main(args)
--- sortroms/__main__.py
+++ sortroms/__main__.py
@@ -1,19 +1,5 @@
-from sortroms import main
+from sortroms import command_line_entrypoint
 import argparse
 
-parser = argparse.ArgumentParser(
-	description='Sort emulator ROM files',
-	prog='sortroms'
-)
-
-parser.add_argument(
-	'folders',
-	metavar='DIR',
-	type=str,
-	nargs='*',
-	help='The ROM folders to sort (absolute)'
-)
-
 if __name__ == '__main__':
-	args = parser.parse_args()
-	main(args)
+	command_line_entrypoint()
